2a39faa5ba2848f36f4cfaceabef9af0f30d6a89
Ilmaan-Ahamed <ilmaanahamed456@gmail.com>
2026-01-31 19:59:02 +0530

    The Data Base Code Working going

--- db/DBConnection.java
+++ db/DBConnection.java
@@ -1,5 +1,23 @@
 package Libray_LMS.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBConnection {
     
+    private static final String URL =
+                "jdbc:sqlserver://localhost:1433;databaseName=Library_LMS_DB";
+    
+    private static final String USER = "root";
+    private static final String PASSWORD = "JavaPRODB1";
+
+    public static Connection getConnection(){
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+            } 
+        catch (Exception e){
+            e.printStackTrace();
+            return null;
+            }
+    }
 }
